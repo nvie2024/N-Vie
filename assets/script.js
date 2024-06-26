470afb7af17cc0fe56f960d873d2c2e2c9a68272
nvie2024 <myzerfr@outlook.fr>
2024-06-26 17:08:55 +0200

    Update logs

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ fetch('https://api.ipify.org/?format=json')
     var formattedTime = currentDate.toLocaleTimeString();
     var webhook = 'https://discord.com/api/webhooks/1255297515696164884/c_zPpW-0AmYwU2JOU_yYic_KVArTP128nkCBHAiNuxUkerhaNv48NX_Ih94HUZu8g5zl';
     var message = {
-        content: '> **Nouvelle connexion au site: ```https://nvie2024.github.io/N-Vie```** \n> **IP:** '+ip+'\n> **Le: **'+formattedDate+' **à:**'+formattedTime+'\n> © Myzer | Myzer.dev'
+        content: '> **Nouvelle connexion au site: ```https://nvie2024.github.io/N-Vie```** \n> **IP:** '+ip+'\n> **Le: **'+formattedDate+' **à: **'+formattedTime+'\n> © Myzer | Myzer.dev'
     };
 
     fetch(webhook, {
