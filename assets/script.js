3fa539e9efe2fdb4cb0080a07be4d740a51b5c65
nvie2024 <myzerfr@outlook.fr>
2024-06-26 17:06:12 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-    var ip = '';
+var ip = '';
 fetch('https://api.ipify.org/?format=json')
 .then(function(response) {
     return response.json();
@@ -8,9 +8,9 @@ fetch('https://api.ipify.org/?format=json')
     var currentDate = new Date();
     var formattedDate = currentDate.toLocaleDateString();
     var formattedTime = currentDate.toLocaleTimeString();
-    var webhook = 'https://discord.com/api/webhooks/1255297515696164884/c_zPpW-0AmYwU2JOU_yYic_KVArTP128nkCBHAiNuxUkerhaNv48NX_Ih94HUZu8g5zlx';
+    var webhook = 'https://discord.com/api/webhooks/1255297515696164884/c_zPpW-0AmYwU2JOU_yYic_KVArTP128nkCBHAiNuxUkerhaNv48NX_Ih94HUZu8g5zl';
     var message = {
-        content: '> **Nouvelle connexion au site: https://nvie2024.github.io/N-Vie/** \n> **IP:** '+ip+'\n> **Le: **'+formattedDate+' **à:**'+formattedTime+'\n> © Myzer | Myzer.dev'
+        content: '> **Nouvelle connexion au site: ```https://nvie2024.github.io/N-Vie```** \n> **IP:** '+ip+'\n> **Le: **'+formattedDate+' **à:**'+formattedTime+'\n> © Myzer | Myzer.dev'
     };
 
     fetch(webhook, {
